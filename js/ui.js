c458ef413ec87619f8b0ae8ec123dccc7ecf5733
basiclines <ismael@basiclines.com>
2013-02-13 17:00:46 +0100

    Wrapped function and some swipeend improves

--- js/ui.js
+++ js/ui.js
@@ -2,123 +2,127 @@
 
 var uiHandlers = function() {
 
-	var context = document.getElementById("canvas");
-	var current = context.querySelector(".view.current");
-	var next = current.nextElementSibling;
-	var prev = current.previousElementSibling;
-	var reload = document.getElementById("reload");
-	var pressEvent =  ("ontouchstart" in window) ? "touchstart" : "mousedown";
-	var moveEvent = ("ontouchstart" in window) ? "touchmove" : "mousemove";
-	var releaseEvent = ("ontouchstart" in window) ? "touchend" :"mouseup";
-	var coordinates = { init: 0, current: 0 }
-	var OFFSET = 25*window.innerWidth/320;
+	var initSwipe = function() {
+		var context = document.getElementById("canvas");
+		var current = context.querySelector(".view.current");
+		var next = current.nextElementSibling;
+		var prev = current.previousElementSibling;
+		var reload = document.getElementById("reload");
+		var pressEvent =  ("ontouchstart" in window) ? "touchstart" : "mousedown";
+		var moveEvent = ("ontouchstart" in window) ? "touchmove" : "mousemove";
+		var releaseEvent = ("ontouchstart" in window) ? "touchend" :"mouseup";
+		var coordinates = { init: 0, current: 0 }
+		var OFFSET = 25*window.innerWidth/320;
 
 
-	// Reset to default state
-	function resetViews () {
-		if (prev) {
-			prev.classList.remove("notransition");
-			prev.setAttribute("style", "");
-		}
-		if (next) {
-			next.classList.remove("notransition");
-			next.setAttribute("style", "");
-		}
-		current.classList.remove("notransition");
-		current.setAttribute("style", "");
-	}
-
-	function move(e) {
-		coordinates.current = (e.touches) ? e.touches[0].pageX : e.clientX;
-
-		if (coordinates.init - coordinates.current >= 0) {
-			// To start
-			coordinates.direction = "start";
-			if (next) {
-				next.classList.add("notransition");
-				var amount = window.innerWidth - OFFSET - (coordinates.init - coordinates.current);
-				next.style.transform = "translateX("+amount+"px)";
-				current.style.opacity = (amount/window.innerWidth)
-			}
-
-		} else {
-			// To end
-			coordinates.direction = "end";
+		// Reset to default state
+		function resetViews () {
 			if (prev) {
-				current.classList.add("notransition");
-				var amount =  coordinates.current - coordinates.init;
-				current.style.transform = "translateX("+amount+"px)";
-				prev.style.opacity = (amount/window.innerWidth)
+				prev.classList.remove("notransition");
+				prev.setAttribute("style", "");
 			}
+			if (next) {
+				next.classList.remove("notransition");
+				next.setAttribute("style", "");
+			}
+			current.classList.remove("notransition");
+			current.setAttribute("style", "");
 		}
 
-	}
+		function move(e) {
+			coordinates.current = (e.touches) ? e.touches[0].pageX : e.clientX;
 
-	function start() {
-		current = context.querySelector(".view.current");
-		next = current.nextElementSibling;
-		prev = current.previousElementSibling;
-
-		context.addEventListener(moveEvent, move);
-		context.addEventListener(releaseEvent, end);
-	}
-
-	function end() {
-		resetViews();
-
-		// // Swipe to start to start or end
-		if ( coordinates.direction == "end" ) {
-			if ( coordinates.current  >= window.innerWidth / 1.5 && prev ) {
-				current.dataset.viewport = "end";
+			if (coordinates.init - coordinates.current >= 0) {
+				// To start
+				coordinates.direction = "start";
 				if (next) {
-					next.classList.remove("next");
+					next.classList.add("notransition");
+					var amount = window.innerWidth - OFFSET - (coordinates.init - coordinates.current);
+					next.style.transform = "translateX("+amount+"px)";
+					current.style.opacity = (amount/window.innerWidth)
 				}
-				current.classList.remove("current")
-				current.classList.add("next")
-				current.previousElementSibling.classList.add("current");
-			}
-		} else {
-			if ( coordinates.current  <= window.innerWidth / 1.5 && next ) {
-				next.classList.remove("next");
-				current.classList.remove("current");
 
-				// Check for last slide
-				if (next.nextElementSibling) {
-					next.classList.add("current");
-					next.nextElementSibling.classList.add("next");
-				} else {
-					next.classList.add("current");
-					next.classList.add("last")
-				}
-				next.dataset.viewport = "";
+			} else {
+				// To end
+				coordinates.direction = "end";
 				if (prev) {
-					prev.style.opacity = 0;
+					current.classList.add("notransition");
+					var amount =  coordinates.current - coordinates.init;
+					current.style.transform = "translateX("+amount+"px)";
+					prev.style.opacity = (amount/window.innerWidth)
 				}
-				current.style.opacity = 0;
-			} else if (next) {
-				next.dataset.viewport = "end";
 			}
+
 		}
 
-		if (next) {
-			// Check for last slide and show reload
-			next.addEventListener("transitionend", function transEnd() {
+		function start() {
+			current = context.querySelector(".view.current");
+			next = current.nextElementSibling;
+			prev = current.previousElementSibling;
 
-				if (next.classList.contains("last")) {
-					reload.classList.remove("hidden")
+			context.addEventListener(moveEvent, move);
+			context.addEventListener(releaseEvent, end);
+		}
+
+		function end() {
+			resetViews();
+
+			// // Swipe to start to start or end
+			if ( coordinates.direction == "end" ) {
+				if ( coordinates.current  >= window.innerWidth / 3 && prev ) {
+					current.dataset.viewport = "end";
+					if (next) {
+						next.classList.remove("next");
+					}
+					current.classList.remove("current")
+					current.classList.add("next")
+					current.previousElementSibling.classList.add("current");
 				}
-				this.removeEventListener("transitionend", transEnd)
-			});
+			} else {
+				if ( coordinates.current  <= window.innerWidth / 1.5 && next ) {
+					next.classList.remove("next");
+					current.classList.remove("current");
+
+					// Check for last slide
+					if (next.nextElementSibling) {
+						next.classList.add("current");
+						next.nextElementSibling.classList.add("next");
+					} else {
+						next.classList.add("current");
+						next.classList.add("last")
+					}
+					next.dataset.viewport = "";
+					if (prev) {
+						prev.style.opacity = 0;
+					}
+					current.style.opacity = 0;
+				} else if (next) {
+					next.dataset.viewport = "end";
+				}
+			}
+
+			if (next) {
+				// Check for last slide and show reload
+				next.addEventListener("transitionend", function transEnd() {
+
+					if (next.classList.contains("last")) {
+						reload.classList.remove("hidden")
+					}
+					this.removeEventListener("transitionend", transEnd)
+				});
+			}
+
+			context.removeEventListener(moveEvent, move, false);
+			context.removeEventListener(releaseEvent, end, false);
 		}
 
-		context.removeEventListener(moveEvent, move, false);
-		context.removeEventListener(releaseEvent, end, false);
+		context.addEventListener(pressEvent, function(e) {
+			coordinates.init = (e.touches) ? e.touches[0].pageX : e.clientX;
+			start();
+		});
 	}
 
-	context.addEventListener(pressEvent, function(e) {
-		coordinates.init = (e.touches) ? e.touches[0].pageX : e.clientX;
-		start();
-	});
+	initSwipe();
 
 	reload.addEventListener("click", function(){
 		window.location.href = window.location.href;
