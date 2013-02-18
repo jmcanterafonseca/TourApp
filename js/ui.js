7da9663a426d3833730afab68a72fae81fef3754
basiclines <ismael@basiclines.com>
2013-02-18 16:11:44 +0100

    Added autoplay mode

--- js/ui.js
+++ js/ui.js
@@ -2,22 +2,83 @@
 
 var uiHandlers = function() {
 
+
 	var initSwipe = function() {
 		var context = document.getElementById("canvas");
-		var current = context.querySelector(".view.current");
-		var next = current.nextElementSibling;
-		var prev = current.previousElementSibling;
 		var closeapp = document.getElementById("closeapp");
+		var current, overlayCurrent, positionCurrent, next, overlayNext, positionNext, prev, overlayPrev, positionPrev;
 
+		var repeat, queueRepeat;
 		var pressEvent =  ("ontouchstart" in window) ? "touchstart" : "mousedown";
 		var moveEvent = ("ontouchstart" in window) ? "touchmove" : "mousemove";
 		var releaseEvent = ("ontouchstart" in window) ? "touchend" :"mouseup";
 		var coordinates = { init: 0, current: 0 }
 		var OFFSET = 25 * (window.innerWidth/320);
 
+		var autoPlay = function() {
+
+			var delay = 3500;
+			context.classList.add("autoplay");
+			function nextSlide() {
+				if (next) {
+					// anonymous becomes next
+					if (next.nextElementSibling) {
+						next.nextElementSibling.classList.add("next");
+
+						// Next becomes current
+						next.dataset.viewport = "start";
+						next.classList.remove("next");
+						next.classList.add("current");
+						next.addEventListener("transitionend", function endTrans() {
+							refreshNodes();
+							this.removeEventListener("transitionend", endTrans);
+						});
+
+						// Current becomes prev
+						overlayCurrent.style.opacity = "1";
+						current.classList.remove("current");
+						current.classList.add("prev");
+
+						// Prev becomes anonymous
+						if (prev) {
+							prev.classList.remove("prev");
+						}
+					} else {
+						// Last slide and reload app
+						window.location.href = window.location.href
+						clearInterval(repeat);
+					}
+
+				}
+			}
+			repeat = setInterval(function(){
+				nextSlide();
+			}, delay);
+		}
+
+
+		function refreshNodes() {
+			current = context.querySelector(".view.current");
+			if (current) {
+				overlayCurrent = current.querySelector(".overlay");
+				positionCurrent = current.querySelector(".position");
+			}
+
+			next = current.nextElementSibling;
+			if (next) {
+				overlayNext = next.querySelector(".overlay");
+				positionNext = next.querySelector(".position");
+			}
+
+			prev = current.previousElementSibling;
+			if (prev) {
+				overlayPrev = prev.querySelector(".overlay");
+				positionPrev = prev.querySelector(".position");
+			}
+		}
 
 		// Reset to default state
-		function resetViews () {
+		function resetViews() {
 			if (prev) {
 				prev.classList.remove("notransition");
 				prev.setAttribute("style", "");
@@ -42,11 +103,11 @@ var uiHandlers = function() {
 				if (next) {
 					var amount = window.innerWidth - OFFSET - (coordinates.init - coordinates.current);
 					next.style.transform = "translateX("+amount+"px)";
-					current.querySelector(".overlay").style.background = "#000";
-					current.querySelector(".overlay").style.opacity = (1.3-amount/window.innerWidth);
-					next.querySelector(".overlay").style.opacity = (amount/window.innerWidth);
-					next.querySelector(".position").style.transform = "translateX("+((window.innerWidth-amount-1)/10)+"px)";
-					next.querySelector(".position").style.opacity = ((amount/2)/window.innerWidth);
+					overlayCurrent.style.background = "#000";
+					overlayCurrent.style.opacity = (1.3-amount/window.innerWidth < 1) ? 1.3-amount/window.innerWidth : 1 ;
+					overlayNext.style.opacity = (amount/window.innerWidth);
+					positionNext.style.transform = "translateX("+((window.innerWidth-amount-1)/10)+"px)";
+					positionNext.style.opacity = ((amount/2)/window.innerWidth);
 				}
 
 			} else {
@@ -55,10 +116,10 @@ var uiHandlers = function() {
 				if (prev) {
 					var amount =  coordinates.current - coordinates.init;
 					current.style.transform = "translateX("+amount+"px)";
-					current.querySelector(".overlay").style.background = "rgba(255,255,255,0.2)";
-					current.querySelector(".overlay").style.opacity = (amount/window.innerWidth);
-					current.querySelector(".position").style.transform = "translateX("+((window.innerWidth-amount-1)/10)+"px)";
-					current.querySelector(".position").style.opacity = (amount/window.innerWidth);
+					overlayPrev.style.background = "rgba(255,255,255,0.2)";
+					overlayPrev.style.opacity = (1.3-amount/window.innerWidth < 1) ? 1.3-amount/window.innerWidth : 1 ;
+					positionCurrent.style.transform = "translateX("+((window.innerWidth-amount-1)/10)+"px)";
+					positionCurrent.style.opacity = (amount/window.innerWidth);
 					if (next) {
 						next.classList.remove("next")
 					}
@@ -68,15 +129,16 @@ var uiHandlers = function() {
 		}
 
 		function start() {
-			current = context.querySelector(".view.current");
+			clearInterval(repeat);
+			clearTimeout(queueRepeat);
+			context.classList.remove("autoplay");
+			refreshNodes();
 			if (current) {
 				current.classList.add("notransition");
 			}
-			next = current.nextElementSibling;
 			if (next) {
 				next.classList.add("notransition");
 			}
-			prev = current.previousElementSibling;
 
 			context.addEventListener(moveEvent, move);
 			context.addEventListener(releaseEvent, end);
@@ -84,6 +146,10 @@ var uiHandlers = function() {
 
 		function end() {
 			resetViews();
+			queueRepeat = setTimeout(function(){
+				refreshNodes();
+				autoPlay();
+			}, 3500);
 
 			// // Swipe to start to start or end
 			if ( coordinates.direction == "end" ) {
@@ -92,25 +158,27 @@ var uiHandlers = function() {
 					if (next) {
 						next.classList.remove("next");
 					}
-					prev.querySelector(".overlay").style.opacity = 0;
-					current.querySelector(".overlay").style.opacity = 1;
-					current.querySelector(".position").style.opacity = 1;
-					current.querySelector(".position").style.transform = "translateX(0px)";
+					overlayPrev.style.opacity = 0;
+					overlayCurrent.style.opacity = 1;
+					positionCurrent.style.opacity = 1;
+					positionCurrent.style.transform = "translateX(0px)";
 					current.classList.remove("current")
 					current.classList.add("next")
 					prev.classList.add("current");
 					prev.classList.remove("prev");
-					if (prev.previousElementSibling) {prev.previousElementSibling.classList.add("prev")}
+					if (prev.previousElementSibling) {
+						prev.previousElementSibling.classList.add("prev");
+					}
 				} else {
 					//Not moved
-					current.querySelector(".position").style.opacity = 0;
+					positionCurrent.style.opacity = 0;
 				}
 			} else {
 				if ( coordinates.current  <= window.innerWidth / 1.5 && next ) {
 					if (prev) {
 						prev.classList.remove("prev");
 					}
-					next.querySelector(".overlay").style.opacity = 0;
+					overlayNext.style.opacity = 0;
 					next.classList.remove("next");
 					current.classList.remove("current");
 					current.classList.add("prev");
@@ -126,9 +194,9 @@ var uiHandlers = function() {
 					next.dataset.viewport = "start";
 				} else if (next) {
 					//Not moved
-					current.querySelector(".overlay").style.opacity = 0;
-					next.querySelector(".position").style.opacity = 1;
-					next.querySelector(".position").style.transform = "translateX(0px)";
+					overlayCurrent.style.opacity = 0;
+					positionNext.style.opacity = 1;
+					positionNext.style.transform = "translateX(0px)";
 					next.dataset.viewport = "end";
 				}
 			}
@@ -141,6 +209,8 @@ var uiHandlers = function() {
 			coordinates.init = (e.touches) ? e.touches[0].pageX : e.clientX;
 			start();
 		});
+		refreshNodes();
+		autoPlay();
 	}
 
 	initSwipe();
