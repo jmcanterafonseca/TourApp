721e6167fb35bc2f4c83e2377414cd4c4d366788
basiclines <igonzaleznicolas@gmail.com>
2013-06-06 17:24:05 +0200

    Added wakeLock and generated new builds

--- js/ready.js
+++ js/ready.js
@@ -1,3 +1,9 @@
+var lock = window.navigator.requestWakeLock('screen');
+
+window.addEventListener('unload', function () {
+    lock.unlock();
+});
+
 // Define global key elements  and initialize UI listeners
 document.addEventListener("DOMContentLoaded", function () {
 
