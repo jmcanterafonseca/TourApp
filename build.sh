ec205f53351814832b92b702d9c91683038ccf8a
basiclines <igonzaleznicolas@gmail.com>
2013-06-20 10:47:13 +0200

    Added remote server structure with build scripts and config files

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+if [ $# != 2 ];then
+	echo "usage: \"$0 <country> <device>\" to generate an application zip" >&2
+	exit -1
+fi
+
+country=$1
+device=$2
+variant=${country}/${device}
+pathSharedBase="/var/www/html/downloads"
+INITIALWD=`pwd`
+
+mkdir -p ${variant}/build/
+cp -r basePack ${variant}/build/package
+
+#cat config.json | sed 's/${VARIANT}/'${country}'\/'${device}'/' >> ${variant}/build/package/config.json
+cat config.json | sed 's/${VARIANT}/'${variant//\//\\\/}'/' >> ${variant}/build/package/config.json
+
+cd ${variant}/build/package/
+zip -r ../package.zip *
+cd ..
+rm -rf package
+
+zipSize=`stat -c %s package.zip`
+
+cd $INITIALWD
+
+cat package.manifest | sed 's/${VARIANT}/'${variant//\//\\\/}'/' | sed 's/${ZIPSIZE}/'${zipSize}'/' >> ${variant}/build/package.manifest
+
+cat index.html | sed 's/${VARIANT}/'${variant//\//\\\/}'/' >> ${variant}/index.html
+
+cp -r $country ${pathSharedBase}/
+
+rm -rf $country
+
+
